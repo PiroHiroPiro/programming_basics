6b1a364fb2f4b33bbbf43d6a62d617c9974b9cc3
PiroHiroPiro <hirohiro11071107@gmail.com>
2020-07-17 01:58:16 +0900

    :+1: 5.4

---
+++ source/5/5.4.ml
@@ -0,0 +1,8 @@
+(* 目的：a * x ^ 2 + b * x + c = 0 が与えられたとき、判別式を求める *)
+(* hanbetsushiki : int -> int -> int -> int *)
+let hanbetsushiki a b c = (b * b) - 4 * a * c
+
+(* テスト *)
+let test1 = hanbetsushiki 2 4 4 = -16
+let test2 = hanbetsushiki 2 4 (-1) = 24
+let test3 = hanbetsushiki 1 2 1 = 0

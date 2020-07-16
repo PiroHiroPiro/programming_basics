f741274f28bdd9a44a65b19ce8e8236ac491700a
PiroHiroPiro <hirohiro11071107@gmail.com>
2020-07-17 01:56:34 +0900

    :+1: kyuyo

---
+++ source/5/kyuyo.ml
@@ -0,0 +1,17 @@
+(* 時給（円） *)
+let jikyu = 950
+
+(* 優遇時給（円） *)
+let yugu_jikyu = 980
+
+(* 基本給（円） *)
+let kihonkyu = 100
+
+(* 目的：働いた時間 x に応じたアルバイト代を計算する *)
+(* kyuyo : int -> int *)
+let kyuyo x =kihonkyu + x * (if x < 30 then jikyu else yugu_jikyu)
+
+(* テスト *)
+let test1 = kyuyo 25 = 23850
+let test2 = kyuyo 28 = 26700
+let test3 = kyuyo 31 = 30480

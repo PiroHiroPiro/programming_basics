12e8b154f690ebc1529b50bdec07069e27103d13
PiroHiroPiro <hirohiro11071107@gmail.com>
2020-07-16 20:09:50 +0900

    :+1: use let in 4.1

--- source/4/4.1.ml
+++ source/4/4.1.ml
@@ -1,2 +1,3 @@
-let baito_kyuyo nenme jikan = (850 * (nenme - 1)) * jikan ;;
+let kihonkyu = 850 ;;
+let baito_kyuyo nenme jikan = (kihonkyu * (nenme - 1)) * jikan ;;
 Printf.printf "%d\n" (baito_kyuyo 2 2) ;;

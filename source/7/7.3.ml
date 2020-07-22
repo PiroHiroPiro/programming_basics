3ebcc892ae3b2f92fc401c82f6d73f5d4fa809d7
PiroHiroPiro <hirohiro11071107@gmail.com>
2020-07-23 01:13:52 +0900

    :+1: 7.3

--- source/7/7.3.ml
+++ source/7/7.3.ml
@@ -1,8 +1,8 @@
 (* 目的：座標を受け取り、x 軸に対象な点の座標を返す *)
-(* taisho_x : int * int -> int * int  *)
-let taisho_x zahyo = match zahyo with x, y -> (x, 0 - y)
+(* taisho_x : float * float -> float * float  *)
+let taisho_x zahyo = match zahyo with x, y -> (x, 0. -. y)
 
 (* テスト *)
-let test1 = taisho_x (2, 6) = (2, -6)
-let test2 = taisho_x (5, 0) = (5, 0)
-let test3 = taisho_x (-3, -2) = (-3, 2)
+let test1 = taisho_x (2., 6.) = (2., -6.)
+let test2 = taisho_x (5.1, 0.) = (5.1, 0.)
+let test3 = taisho_x (-3., -2.4) = (-3., 2.4)

568ee5b663eec4c32f39cc469c3b158e76b8742e
PiroHiroPiro <hirohiro11071107@gmail.com>
2020-07-23 01:40:12 +0900

    :+1: 8.3

--- source/8/8.3.ml
+++ source/8/8.3.ml
@@ -3,8 +3,9 @@ type person_t = {
   hight: float ;
   weight: float ;
   birthday: string ;
+  bloodtype: string ;
 }
 
-let person1 = {name="たかし" ; hight=1.65 ; weight=55. ; birthday="1992/08/07" ;}
-let person2 = {name="えみ" ; hight=1.42 ; weight=50.1 ; birthday="1998/10/21" ;}
-let person3 = {name="とおる" ; hight=1.85 ; weight=69.3 ; birthday="1995/01/06" ;}
+let person1 = {name="たかし" ; hight=1.65 ; weight=55. ; birthday="1992/08/07" ; bloodtype="A" ;}
+let person2 = {name="えみ" ; hight=1.42 ; weight=50.1 ; birthday="1998/10/21" ; bloodtype="O" ;}
+let person3 = {name="とおる" ; hight=1.85 ; weight=69.3 ; birthday="1995/01/06" ; bloodtype="B" ;}

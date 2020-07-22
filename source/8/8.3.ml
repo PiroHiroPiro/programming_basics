92f9dc58d622d7f472c5e7170b1977f2885524fe
PiroHiroPiro <hirohiro11071107@gmail.com>
2020-07-23 01:35:29 +0900

    :+1: 8.3

---
+++ source/8/8.3.ml
@@ -0,0 +1,10 @@
+type person_t = {
+  name: string ;
+  hight: float ;
+  weight: float ;
+  birthday: string ;
+}
+
+let person1 = {name="たかし" ; hight=1.65 ; weight=55. ; birthday="1992/08/07" ;}
+let person2 = {name="えみ" ; hight=1.42 ; weight=50.1 ; birthday="1998/10/21" ;}
+let person3 = {name="とおる" ; hight=1.85 ; weight=69.3 ; birthday="1995/01/06" ;}

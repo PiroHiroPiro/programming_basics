22fc982af3283921d79642ba9db220099a5e25b9
PiroHiroPiro <hirohiro11071107@gmail.com>
2020-07-23 01:59:45 +0900

    :+1: 8.6

--- source/8/metro_network.ml
+++ source/8/metro_network.ml
@@ -5,3 +5,17 @@ type ekimei_t = {
   romaji_name: string ; (* ローマ字の駅 *)
   rosen_name: string ; (* 路線名 *)
 }
+
+(* 目的：駅名のデータを受け取り、「路線名、駅名（かな）」の形式の文字列を返す *)
+(* hyoji : ekimei_t -> string *)
+let hyoji ekimei = match ekimei with
+  {kanji_name=kanji_name ; hiragana_name=hiragana_name ; romaji_name=romaji_name ; rosen_name=rosen_name ;} ->
+  rosen_name ^ "、" ^ kanji_name ^ "（" ^ hiragana_name ^ "）"
+
+(* テスト *)
+let ekimei1 = {kanji_name="茗荷谷" ; hiragana_name="みょうがだに" ; romaji_name="myogadani" ; rosen_name="丸ノ内線" ;}
+let ekimei2 = {kanji_name="新宿" ; hiragana_name="しんじゅく" ; romaji_name="shinjuku" ; rosen_name="山の手線" ;}
+let ekimei3 = {kanji_name="東京" ; hiragana_name="とうきょう" ; romaji_name="tokyo" ; rosen_name="山の手線" ;}
+let test1 = hyoji ekimei1 = "丸ノ内線、茗荷谷（みょうがだに）"
+let test2 = hyoji ekimei2 = "山の手線、新宿（しんじゅく）"
+let test3 = hyoji ekimei3 = "山の手線、東京（とうきょう）"

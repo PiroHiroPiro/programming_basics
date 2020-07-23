64087285ae3d1693bf3728ac6132288c1b84f89a
PiroHiroPiro <hirohiro11071107@gmail.com>
2020-07-23 16:08:52 +0900

    :wrench: remove space before ;

--- source/8/8.1.ml
+++ source/8/8.1.ml
@@ -1,11 +1,11 @@
 type book_t = {
-  title: string ;
-  author: string ;
-  publisher: string ;
-  price: int ;
-  isbn: string ;
+  title: string;
+  author: string;
+  publisher: string;
+  price: int;
+  isbn: string;
 }
 
-let book1 = {title="プログラミングの基礎" ; author="浅井健一" ; publisher="サイエンス社" ; price=2530 ; isbn="978-4-7819-1160-1" ;}
-let book2 = {title="リーダブルコード" ; author="Dustin Boswell and Trevor Foucher" ; publisher="オライリー・ジャパン" ; price=2640 ; isbn="978-4-87311-565-8" ;}
-let book3 = {title="ネットワークスペシャリスト" ; author="ICTワークショップ" ; publisher="翔泳社" ; price=3168 ; isbn="978-4-7981-6558-5" ;}
+let book1 = {title="プログラミングの基礎"; author="浅井健一"; publisher="サイエンス社"; price=2530; isbn="978-4-7819-1160-1";}
+let book2 = {title="リーダブルコード"; author="Dustin Boswell and Trevor Foucher"; publisher="オライリー・ジャパン"; price=2640; isbn="978-4-87311-565-8";}
+let book3 = {title="ネットワークスペシャリスト"; author="ICTワークショップ"; publisher="翔泳社"; price=3168; isbn="978-4-7981-6558-5";}
--- source/8/8.2.ml
+++ source/8/8.2.ml
@@ -1,10 +1,10 @@
 type okozukai_t = {
-  name: string ;
-  price: int ;
-  location: string ;
-  date: string ;
+  name: string;
+  price: int;
+  location: string;
+  date: string;
 }
 
-let okozukai1 = {name="プログラミングの基礎" ; price=2530 ; location="Amazon" ; date="2019/10/26" ;}
-let okozukai2 = {name="リーダブルコード" ; price=2640 ; location="Amazon" ; date="2018/04/01" ;}
-let okozukai3 = {name="ネットワークスペシャリスト" ; price=3168 ; location="Amazon" ; date="2020/05/10" ;}
+let okozukai1 = {name="プログラミングの基礎"; price=2530; location="Amazon"; date="2019/10/26";}
+let okozukai2 = {name="リーダブルコード"; price=2640; location="Amazon"; date="2018/04/01";}
+let okozukai3 = {name="ネットワークスペシャリスト"; price=3168; location="Amazon"; date="2020/05/10";}

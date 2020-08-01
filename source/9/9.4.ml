16d4b3602d50ce83d18d816ef9c6f0753d309655
PiroHiroPiro <hirohiro11071107@gmail.com>
2020-08-01 10:10:06 +0900

    :+1: 9.4

--- source/9/9.4.ml
+++ source/9/9.4.ml
@@ -6,3 +6,5 @@ let rec length lst = match lst with
 
 (* テスト *)
 let test1 = length [2; 1; 6; 4; 7] = 5
+let test2 = length ["春"; "夏"; "秋"; "冬"] = 4 (* List という型であれば何でも良いよいう認識で OK? *)
+let test3 = length [] = 0

0f0aa3b60f84c70cce2009e7801bb128d0edd4ff
PiroHiroPiro <hirohiro11071107@gmail.com>
2020-08-01 14:53:25 +0900

    :+1: 10.12

--- source/10/metro_network.ml
+++ source/10/metro_network.ml
@@ -384,3 +384,19 @@ let rec get_ekikan_kyori eki1 eki2 ekikan_list = match ekikan_list with
 
 (* テスト *)
 let test2 = get_ekikan_kyori "茗荷谷" "新大塚" global_ekikan_list = 1.2
+
+(* 目的：ローマ字の駅名二つを受け取り、駅間の距離を表す文字列を返す *)
+(* kyori_wo_hyoji : string -> string -> string *)
+let kyori_wo_hyoji romaji_ekimei1 romaji_ekimei2 = 
+  let kanji_ekimei1 = romaji_to_kanji romaji_ekimei1 global_ekimei_list in
+  let kanji_ekimei2 = romaji_to_kanji romaji_ekimei2 global_ekimei_list in
+  if kanji_ekimei1 = "" then romaji_ekimei1 ^ "という駅は存在しません"
+  else if kanji_ekimei2 = "" then romaji_ekimei2 ^ "という駅は存在しません"
+  else let ekikan_kyori = get_ekikan_kyori kanji_ekimei1 kanji_ekimei2 global_ekikan_list in
+    if ekikan_kyori = infinity then kanji_ekimei1 ^ "駅と" ^ kanji_ekimei2 ^ "駅はつながっていません"
+    else kanji_ekimei1 ^ "駅から" ^ kanji_ekimei2 ^ "駅までは" ^ string_of_float ekikan_kyori ^ "kmです"
+
+(* テスト *)
+let test3 = kyori_wo_hyoji "myogadani" "shinotsuka" = "茗荷谷駅から新大塚駅までは1.2kmです"
+let test4 = kyori_wo_hyoji "myogadani" "tameikesanno" = "茗荷谷駅と溜池山王駅はつながっていません"
+let test5 = kyori_wo_hyoji "myogadani" "nagaokakyo" = "nagaokakyoという駅は存在しません"

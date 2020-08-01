f355ee14511799de187fcd3f04a3791ff8c25aed
PiroHiroPiro <hirohiro11071107@gmail.com>
2020-08-01 15:07:20 +0900

    :+1: 11.2

---
+++ source/11/11.2.ml
@@ -0,0 +1,10 @@
+(* 目的：漸化式で定義される数列の第 n 項を求める *)
+(* a : int -> int *)
+let rec a n =
+  if n = 0 then 3
+  else 2 * a (n - 1) - 1
+
+(* テスト *)
+let test1 = a 0 = 3
+let test2 = a 2 = 9
+let test3 = a 5 = 65

9b4788858a47d1162c845c05e6da97d3cac27994
PiroHiroPiro <hirohiro11071107@gmail.com>
2020-08-16 17:32:40 +0900

    :+1: 14.14

---
+++ source/14/14.14.ml
@@ -0,0 +1,12 @@
+(* 目的：init から始めて lst の要素を右から順に f を施し込む*)
+(* fold_right : ('a -> 'b -> 'b) -> 'a list -> 'b -> 'b *)
+let rec fold_right f lst init = match lst with
+    [] -> init
+  | first :: rest -> f first (fold_right f rest init)
+
+(* 目的：文字列のリストを受け取り、要素を前から順に繋げた文字列を返す *)
+(* concat : string list -> string *)
+let concat lst = fold_right (^) lst ""
+
+(* テスト *)
+let test1 = concat ["春"; "夏"; "秋"; "冬"] = "春夏秋冬"

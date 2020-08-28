f8cc9958e77f4a50d3611e9a4228c01c686be7ae
PiroHiroPiro <hirohiro11071107@gmail.com>
2020-08-29 01:28:50 +0900

    :+1: 15.2

---
+++ source/15/15.2.ml
@@ -0,0 +1,9 @@
+(* 目的：二つの自然数（m >= n >= 0）を受け取り、ユークリッドの互除法を使って最大公約数を返す *)
+(* gcd : int -> int -> int *)
+let rec gcd m n =
+  if n == 0 then m
+            else gcd n (m mod n)
+
+(* テスト：gcd *)
+let test1 = gcd 45 25 = 5
+let test2 = gcd 111 7 = 1

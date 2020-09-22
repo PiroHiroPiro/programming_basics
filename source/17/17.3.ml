6e5a768d4cee71ce9d3c8ef7cf9c320edf9f00fc
PiroHiroPiro <hirohiro11071107@gmail.com>
2020-09-22 19:45:38 +0900

    :+1: 17.3

---
+++ source/17/17.3.ml
@@ -0,0 +1,13 @@
+(* 星座を表す型 *)
+type seiza_t = Aries
+             | Taurus
+             | Gemini
+             | Cancer
+             | Leo
+             | Virgo
+             | Libra
+             | Scorpio
+             | Sagittarius
+             | Capricorn
+             | Aquarius
+             | Pisces

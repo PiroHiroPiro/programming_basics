8e93c0f41249e960c7c72df77cfae5283f76888a
PiroHiroPiro <hirohiro11071107@gmail.com>
2020-09-22 19:43:01 +0900

    :+1: 17.2

---
+++ source/17/17.2.ml
@@ -0,0 +1,13 @@
+(* 年月を表す型 *)
+type year_t = January of int
+            | February of int
+            | March of int
+            | April of int
+            | May of int
+            | June of int
+            | July of int
+            | August of int
+            | September of int
+            | October of int
+            | November of int
+            | December of int

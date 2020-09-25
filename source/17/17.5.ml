aa6ebc15b450013e5f17a70b556440c61253c565
PiroHiroPiro <hirohiro11071107@gmail.com>
2020-09-26 00:37:18 +0900

    :wrench: remove unnecessary code

--- source/17/17.5.ml
+++ source/17/17.5.ml
@@ -15,19 +15,6 @@ let tree2 = Leaf (3)
 let tree3 = Node (tree1, 4, tree2)
 let tree4 = Node (tree2, 5, tree3)
 
-(* 目的：tree に含まれる整数をすべて加える *)
-(* sum_tree : tree_t -> int *)
-let rec sum_tree tree = match tree with
-    Empty -> 0
-  | Leaf (n) -> n
-  | Node (t1, n, t2) -> sum_tree t1 + n + sum_tree t2
-
-(* テスト：sum_tree *)
-let test1 = sum_tree tree1 = 0
-let test2 = sum_tree tree2 = 3
-let test3 = sum_tree tree3 = 7
-let test4 = sum_tree tree4 = 15
-
 (* 目的：tree に含まれる整数を二倍にした木を返す *)
 (* tree_double : tree_t -> tree_t *)
 let rec tree_double tree = match tree with

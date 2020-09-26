6942ac5567cb07a6b4d6cfb05bf3a75c6bdd7b54
PiroHiroPiro <hirohiro11071107@gmail.com>
2020-09-26 18:01:28 +0900

    :+1: 17.17

---
+++ source/17/17.17.ml
@@ -0,0 +1,14 @@
+(* 目的：受け取った lst の中の最小の値を返す *)
+(* minimum : int -> int list -> int *)
+let rec minimum min lst = match lst with
+    [] -> min
+  | first :: rest -> let min_rest = minimum first rest in
+      if min <= min_rest
+        then min
+        else min_rest
+
+(* テスト：minimum *)
+let test1 = minimum 3 [] = 3
+let test1 = minimum 1 [2] = 1
+let test1 = minimum 3 [2] = 2
+let test1 = minimum 3 [2; 6; 4; 1; 8] = 1
